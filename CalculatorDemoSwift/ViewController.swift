1af9ef0390fe902473ed3b7a072561f1e91b15af
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2019-03-26 17:50:13 +0200

    Initial Commit

---
+++ CalculatorDemoSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorDemoSwift
+//
+//  Created by Alexandr Marchenko on 3/26/19.
+//  Copyright © 2019 Alexandr Marchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
